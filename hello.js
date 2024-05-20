f7da0693b08c7ebce30f0a189ef54fee19073b77
smfulton <sfulton1996@gmail.com>
2024-05-20 18:45:42 -0500

    now says Hello, World!

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello number "+ parseInt(10-1)+" you should know\nQuoth the Raven \"Nevermore\"\n"+'This "works" as well'+` and "this"`);
+console.log("Hello, World!");
 
 /*  
 Next, PLAY! 
